4f09aca0fbf0beefc61775ed5d8c37c65e74880e
Kilian Authier <kilian.authier@gmail.com>
2025-07-14 16:19:30 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://kilian:<db_password>@cluster0.kkk12ln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
