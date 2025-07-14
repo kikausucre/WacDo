5cd6e63dc77999c5b906d3e161186a70a5b7f6e2
Kilian Authier <kilian.authier@gmail.com>
2025-07-14 17:41:32 +0200

    Premier dépot du projet

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); //active CORS pour toutes les requetes
+
 
 mongoose.connect('mongodb+srv://kilian:<db_password>@cluster0.kkk12ln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   { useNewUrlParser: true,
