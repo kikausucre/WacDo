149e6b7bbe245ce7d9ff6335e4eebba82d5372e7
Kilian Authier <kilian.authier@gmail.com>
2025-07-23 07:07:16 +0200

    frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,29 +7,29 @@ const Menu = require('../models/Menu');
     // Création d'une Commande'
 exports.createOrder = async (req, res) => {
   try {
-    const { produits = [], supplements = [], nomClient, heureLivraison } = req.body;
+    const { produits = [], supplements = [], nomClient, heureLivraison } = req.body; //recupere dans la requete
 
     let total = 0;
 
     
-    for (const item of produits) {
+    for (const item of produits) {//calcul le prix des produits
       if (item.type.toLowerCase() === 'produit') {
         const produit = await Product.findOne({ nom: item.nom });
         if (!produit) continue;
         total += produit.prix * (item.quantite || 1);
 
-      } else if (item.type.toLowerCase() === 'menu') {
+      } else if (item.type.toLowerCase() === 'menu') { // calcul le prix des menus
         const menu = await Menu.findOne({ nom: item.nom });
         if (!menu) continue;
         total += menu.prix * (item.quantite || 1);
       }
     }
 
-    for (const sup of supplements) {
+    for (const sup of supplements) { // calcul les supplements 
       total += (sup.prix || 0) * (sup.quantite || 1);
     }
 
-    const nouvelleOrder = new Order({
+    const nouvelleOrder = new Order({ // cree la commande 
       nomClient,
       heureCommande: new Date(),
       heureLivraison,
@@ -48,10 +48,10 @@ exports.createOrder = async (req, res) => {
 };
 
 
-//recupere la liste des menus dans la DB
+//recupere la liste des commandes dans la DB
 exports.getAllOrder = (req, res) => {
   Order.find()
-    .sort({ datelivraison: 1 }) //1 pour ordre croissant
+    .sort({ heureLivraison: 1 }) //1 pour ordre croissant
     .then(menus => res.status(200).json(menus))
     .catch(error => res.status(400).json({ error }));
 };
